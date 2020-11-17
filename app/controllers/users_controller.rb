72eacb860cb858627738fe4fde09688c2cffd9cf
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-17 19:24:45 +0530

    configure user model and category controller to test first load

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.name.downcase!
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'User created succesfully'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Something went wrong, please check the errors'
+      render :new
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :articles, foreign_key: :author_id, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
 end
