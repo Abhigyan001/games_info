7f6f7a923518e7d18d1d3d5e2bac4bff79cbaca7
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-18 10:33:44 +0530

    modify application controller for sign in functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def display_propper_flash
+    if flash[:notice]
+      content_tag(:div, flash[:notice], class: 'flash notice', align: 'center')
+    elsif flash[:success]
+      content_tag(:div, flash[:success], class: 'flash success', align: 'center')
+    elsif flash[:danger]
+      content_tag(:div, flash[:danger], class: 'flash danger', align: 'center')
+    end
+  end
+
+  def errors_message(object)
+    return unless object.errors.any?
+
+    content_tag(:div, nil, class: 'errors') do
+      content_tag(:p) do
+        "You have the following #{pluralize(object.errors.count, 'error')}"
+      end +
+        content_tag(:ul) do
+          object.errors.full_messages.each do |msg|
+            concat content_tag(:li, "- #{msg}")
+          end
+        end
+    end
+  end
 end
